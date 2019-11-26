ced90eb423e00dd4da6bee67a3b8aa70bafb9d25
VladisKrew <kruglovvladislav@mail.ru>
2019-11-26 10:39:14 +0300

    Start4

--- include/task3.h
+++ include/task3.h
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-unsigned long long sumPrime(unsigned int hbound)
-=======
-unsigned long long sumPrime(unsigned int hbound);
->>>>>>> 47399598bdc9dd509d31bc10c3dc0a4838a270b6
+//Second task function
+unsigned long long sumPrime(unsigned int hbound); 
